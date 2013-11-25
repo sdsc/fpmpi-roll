b7395ea13b9abaab0e5bff7a8fff75078162f0c8
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:55:45 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/fpmpi/version.mk
+++ src/fpmpi/version.mk
@@ -1,3 +1,14 @@
-NAME    = fpmpi_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.2
-RELEASE = 1
+NAME               = fpmpi_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.2
+RELEASE            = 1
+PKGROOT            = /opt/$(ROLLMPI)/$(ROLLCOMPILER)/$(ROLLNETWORK)
+
+SRC_SUBDIR         = fpmpi
+
+SOURCE_NAME        = fpmpi
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
