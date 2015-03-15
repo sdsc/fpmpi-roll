e9f5c9693c08a1ab0daf3232a8fe47c19953a9b3
Jerry Greenberg <jpg@comet-devel.local>
2015-03-14 21:05:22 -0700

    added auto versioning

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = fpmpi
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = fpmpi
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
