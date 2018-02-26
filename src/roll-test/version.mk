e4ecf6b5986d8e608fbc4147d5d32945f8bf44b1
Jim Hayes <jhayes@sdsc.edu>
2018-02-26 11:57:06 -0800

    Set rpm make vars for Rocks v7.
    Resolves APS-681.

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,7 @@
 NAME       = sdsc-fpmpi-roll-test
 VERSION    = 2
-RELEASE    = 2
+RELEASE    = 3
 PKGROOT    = /root/rolltests
 
-RPM.EXTRAS = AutoReq:No
+RPM.EXTRAS = AutoReq:No\nAutoProv:No
+RPM.FILES  = $(PKGROOT)/fpmpi.t
